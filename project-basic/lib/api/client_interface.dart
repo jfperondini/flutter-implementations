f27a17a5997fdf99fedf0cc57864f51da0ff0133
João Fernando Perondini <jfperondini@gmail.com>
2022-11-23 07:56:35 -0300

    first

--- project-basic/lib/api/client_interface.dart
+++
@@ -1,4 +0,0 @@
-abstract class IHttpClient {
-  Future<dynamic> get(String path);
-  Future<dynamic> post(String path, Map map, Map<String, String> headers);
-}
--- project-basic/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:project/app_module.dart';
-import 'package:project/app_widget.dart';
-
-void main() async {
-  runApp(ModularApp(module: ProjectModule(), child: const ProjectApp()));
-}
